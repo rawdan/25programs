859be18ca482a942a4f5341c9d64c2b77f006dd1
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-04 21:22:32 +0300

    improve variable names

--- char2ascii.rb
+++ char2ascii.rb
@@ -3,8 +3,8 @@ class Char2ASCII
   def convert_to_ascii
     puts "Enter any character (or more) to convert them to ascii"
     user_input = gets.chomp
-    user_input.each_byte do |char|
-      puts "The ascii value for '#{char.chr}' is: #{char}"
+    user_input.each_byte do |character|
+      puts "The ascii value for '#{character.chr}' is: #{character}"
     end
   end
 end
