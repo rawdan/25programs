47c11eeb71cc9ce81abc6e1494ff90bc5361b343
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-04 23:16:32 +0300

    refactor: extract method to make logic easier to read

--- armstrong.rb
+++ armstrong.rb
@@ -1,17 +1,23 @@
 class Armstrong
 
 
-  def is_armstrong?
+  def check_user_number
     puts "Enter any number for check: "
-    range = gets.chomp.to_i
-    all_digits = range.to_s.chars.map(&:to_i)
-    raised_to_power = all_digits.map! { |a| a**3 }
-    if raised_to_power.inject(0){|sum,x| sum + x } == range
-      puts "your number #{range} is a narcisistic number"
+    user_input = gets.chomp.to_i
+    is_narcisistic?(user_input)
+  end
+
+  private
+
+  def is_narcisistic?(user_input)
+    all_digits = user_input.to_s.chars.map(&:to_i)
+    raised_to_power = all_digits.map {|a| a ** 3}
+    if raised_to_power.inject(0) {|sum, x| sum + x} == user_input
+      puts "your number #{user_input} is a narcisistic number"
     else
       puts "it's not a narcisistic number"
     end
   end
 end
 
-Armstrong.new.is_armstrong?
+Armstrong.new.check_user_number
