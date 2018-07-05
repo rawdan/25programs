cd769015ac8196318e8e151a8256173b2b997b0d
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-05 21:28:24 +0300

    add method to chose which operation to do

--- bin/calculator.rb
+++ bin/calculator.rb
@@ -7,4 +7,22 @@ class Calculator
     You can choose to 1.add , 2.substract, 3.multiply, 4.divide and 5.modulus"
     name
   end
+
+  def choose_one_operation
+    puts "Chone one operation from: 1 to add, 2 to substract, 3 to multiply, 4 to divide and 5 for modulus"
+    selection = gets.chomp.to_i
+    if selection == 1
+      "add"
+    elsif selection == 2
+      "substract"
+    elsif selection == 3
+      "multiply"
+    elsif selection == 4
+      "divide"
+    elsif selection == 5
+      "modulus"
+    else
+      'error'
+    end
+  end
 end
