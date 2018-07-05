e83fa75dcfbc8c9f01eb01f48fcef82afe73aa6b
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-05 21:39:57 +0300

    make a working calculator

--- bin/calculator.rb
+++ bin/calculator.rb
@@ -1,29 +1,28 @@
-class Calculator
 
-  def greet_user
-    puts "Helo there, what's your name?"
-    name = gets.chomp
-    puts "Pleasure to make your acquaintance, I am here to help you with calculating \n
-    You can choose to 1.add , 2.substract, 3.multiply, 4.divide and 5.modulus"
+  def greeting
+    puts "Hello!  Please type your name: "
+    name = gets
+    puts "It is nice to meet you #{name}.  I am a simple calculator application.  \n
+  I can add, subtract, multiply, and divide.\n"
     name
   end
 
-  def choose_one_operation
-    puts "Chone one operation from: 1 to add, 2 to substract, 3 to multiply, 4 to divide and 5 for modulus"
-    selection = gets.chomp.to_i
-    if selection == 1
+  def request_calculation_type
+    puts "Type 1 to add, 2 to subtract, 3 to multiply, or 4 to divide two numbers: "
+    operation_selection = gets.to_i
+
+    if operation_selection == 1
       "add"
-    elsif selection == 2
-      "substract"
-    elsif selection == 3
+    elsif operation_selection == 2
+      "subtract"
+    elsif operation_selection == 3
       "multiply"
-    elsif selection == 4
+    elsif operation_selection == 4
       "divide"
-    elsif selection == 5
-      "modulus"
     else
       "error"
     end
+
   end
 
   def calculate_answer(operator, a, b)
@@ -35,9 +34,30 @@ class Calculator
       a * b
     elsif operator == "divide"
       a / b
-    elsif operator == "modulus"
-      a % b
     end
   end
 
-end
+  name = greeting
+  run_calculator = 1
+
+  while run_calculator == 1
+
+    current_calculation = request_calculation_type()
+
+    if current_calculation == "error"
+
+      puts "I do not understand this type of calculation... Can we try again?"
+
+    else
+      puts "What is the first number you would you like to #{current_calculation}: "
+      first_number = gets.to_i
+      puts "What is the second number you would like to #{current_calculation}: "
+      second_number = gets.to_i
+
+      answer = calculate_answer(current_calculation, first_number, second_number)
+
+      puts "The answer is #{answer}"
+      puts "Type 1 to run another calcution or 2 to end: "
+      run_calculator = gets.to_i
+    end
+  end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,9 +1,2 @@
 require_relative '../bin/calculator'
 require 'spec_helper'
-
-RSpec.describe Calculator do
-
-  it 'can instantiate a new calculator' do
-    expect(Calculator.new).not_to be nil
-  end
-end
