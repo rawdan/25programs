c9b5065f0943e1c4f2d5ff28566cd02c7f3aa518
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-06 19:20:34 +0300

    write a simple script that checks 2 arrays to see if they are equal in lenght and in contents

--- bin/equal_array.rb
+++ bin/equal_array.rb
@@ -1,13 +1,32 @@
 class EqualArray
 
-  def check_id_equal
+  def check_if_equal
     arr1 = [1,2,3,4,5]
     arr2 = [1,2,3,4,5]
 
+    check_length(arr1, arr2)
+  end
+
+  private
+
+  def check_length(arr1, arr2)
     if arr1.length == arr2.length
       puts "The arrays have the same length"
+      puts "Do you want to check if the contents are the same or not? y/n"
+      answer = gets.chomp.to_s
+      if answer == "y"
+        check_contents(arr1, arr2)
+      end
+    end
+  end
+
+  def check_contents(arr1, arr2)
+    if arr1.sort == arr2.sort
+      puts "The arrays also have the same contents"
+    else
+      puts "They do not have the same contents"
     end
   end
 end
 
-EqualArray.new.check_id_equal
+EqualArray.new.check_if_equal
