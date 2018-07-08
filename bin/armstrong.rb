5b9833a07ec606904929eeaed5a4baa9b8135d80
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-09 00:10:49 +0300

    fix typo

--- bin/armstrong.rb
+++ bin/armstrong.rb
@@ -3,19 +3,19 @@ class Armstrong
   def check_user_number
     puts "Enter any number for check: "
     user_input = gets.chomp.to_i
-    is_narcisistic?(user_input)
+    is_narcissistic?(user_input)
   end
 
   private
 
-  def is_narcisistic?(user_input)
+  def is_narcissistic?(user_input)
     all_digits = user_input.to_s.chars.map(&:to_i)
     number_of_digits = all_digits.count
     raised_to_power = all_digits.map {|a| a ** number_of_digits}
     if raised_to_power.inject(0) {|sum, x| sum + x} == user_input
-      puts "your number #{user_input} is a narcisistic number"
+      puts "your number #{user_input} is a narcissistic number"
     else
-      puts "it's not a narcisistic number"
+      puts "it's not a narcissistic number"
     end
   end
 end
