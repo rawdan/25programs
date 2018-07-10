0ccf3900c5f1a0ba92464af757cfe1a8c325e96a
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-10 20:49:23 +0300

    extract methods to beautify code

--- bin/tip_tax_calculator.rb
+++ bin/tip_tax_calculator.rb
@@ -4,12 +4,23 @@ class TipTaxCalculator
     puts "How much was yor base bill?"
     amount = gets.chomp.to_i
     puts "What percentage are you willing to tip?"
-    tip_procentage = gets.chomp.to_i
-    tip = amount * tip_procentage / 100
-    tax = amount * 10 / 100
+    tip = get_and_calculate_tip_from amount
+    tax = calculate_tax_from amount
     total = amount + tip + tax
     puts "You will have to pay #{total} dollars of which the tip was #{tip} dollars and tax was #{tax} dollars"
   end
+
+  private
+
+  def get_and_calculate_tip_from(amount)
+    tip_procentage = gets.chomp.to_i
+    amount * tip_procentage / 100
+  end
+
+  def calculate_tax_from(amount)
+    amount * 10 / 100
+  end
+
 end
 
 TipTaxCalculator.new.ask_amount
